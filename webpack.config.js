54c44dce528976929b85a51290105fa5dd776a51
fugalfunkster <mtthwhsltn@gmail.com>
2016-08-19 17:04:44 -0700

    remove build and update configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,98 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 const merge = require('webpack-merge');
-const parts = require('./libs/parts');
 const validate = require('webpack-validator');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const cssnext = require('postcss-cssnext');
+const values = require('postcss-modules-values');
+
+// Set up separate paths to our app and build directories
+const path = require('path');
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
+// Shared configuration variables for both deveopment and production environments
 const common = {
-  // Entry accepts a path or an object of entries.
-  // We'll be using the latter form given it's
-  // convenient with more complex configurations.
   entry: {
     app: PATHS.app
   },
   output: {
     path: PATHS.build,
+    // for deployment to gh-pages:
+    publicPath: '/ciderData/',
     filename: '[name].js'
-  }, plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Webpack demo'
-    })
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader'),
+        include: PATHS.app
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        query: {
+          cacheDirectory: true,
+          presets: ['react', 'es2015', 'es2016', {plugins: ['transform-class-properties', 'transform-object-rest-spread']}]
+        },
+        include: PATHS.app
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ title: 'Cider Apple Data' }), // Title of the index.html page for our app
+    new ExtractTextPlugin('style.css', { allChunks: true })
   ],
-  // Important! Do not remove ''. If you do, imports without
-  // an extension won't work anymore!
+  postcss: function () {
+    return [cssnext, values];
+  },
   resolve: {
     extensions: ['', '.js', '.jsx']
   }
 };
 
 
-var config;
 // Detect how npm is run and branch based on that
-switch(process.env.npm_lifecycle_event) {
-  case 'build':
-    config = merge(common,  { devtool: 'source-map' },
-                   parts.setupCSS(PATHS.app),
-                   parts.setupBabelReactES6(PATHS.app));
-    break;
-  default:
-    config = merge(common, { devtool: 'eval-source-map' },
-                   parts.setupCSS(PATHS.app),
-                   parts.setupBabelReactES6(PATHS.app),
-                   parts.devServer({
-                     // Customize host/port here if needed
-                     host: process.env.HOST,
-                     port: process.env.PORT
-                   })
-                  );
+
+var config;
+// Production Mixins
+if (process.env.npm_lifecycle_event === 'build') {
+  config = merge(
+    common,
+    {
+      devtool: 'source-map',
+      plugins: [
+        new webpack.DefinePlugin({'process.env.NODE_ENV': 'production'}),
+        new CleanWebpackPlugin([PATHS.build], {root: process.cwd()}),
+        new webpack.optimize.UglifyJsPlugin({ compress: {warnings: false}})
+      ]
+    }
+  );
+} else {
+  // Development Mixins
+  config = merge(
+    common,
+    { devtool: 'eval-source-map',
+      devServer: {
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        stats: 'errors-only',
+        // Customize host/port here if needed
+        host: process.env.host,  // Defaults to `localhost`
+        port: process.env.port   // Defaults to 8080
+      }, plugins: [
+        new webpack.HotModuleReplacementPlugin({
+          multiStep: true })
+      ]
+    }
+  );
 }
 
 module.exports = validate(config);
